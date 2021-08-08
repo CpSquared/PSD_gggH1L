9a200cbceb13ea491d895d9c0c2cd270a6e8d530
CpSquared <68424678+CpSquared@users.noreply.github.com>
2021-08-09 02:25:29 +0530

    Add files via upload

---
+++ Expansion by regions/g2/myintegrate.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from pySecDec.integral_interface import IntegralLibrary
+import sympy as sp
+import math
+
+# g5
+
+EulerGamma = 0.57721566490153286060651209008240243104215933593992
+
+def g2_0(s12,mh2,mt2):
+    analytic0 = -(6*mt2 + s12)/(12*mt2**2)
+    return analytic0
+
+def g2_eps(s12,mh2,mt2):
+    analytic1 =  -(s12 + (6*mt2 + s12)*math.log(1/mt2))/(12*mt2**2)
+    analytic2 = (g2_0(s12,mh2,mt2)*(-1*EulerGamma) + analytic1)
+    return analytic2
+
+# load c++ library
+#s1, s2, s3, mtsq, mhsq =  0.0009 , -0.000442873775 , -0.0003 , 1.0 , 0.000157126225
+s1, s2, s3, mtsq, mhsq =  0.00383,-0.000966,-0.000966,1.0,0.000157126225
+names = ["g2_s12_z"]
+real_parameters = [[s1,s2,s3,mtsq,mhsq,1]] # z set to 1
+
+#names = ["g7_s12_z", "g7_s12_m"]
+#real_parameters = [[s1,s2,s3,mtsq,mhsq,1], [s1,s2,s3,mtsq,mhsq]] # z set to 1
+
+#EulerGamma = 0.57721566490153286060651209008240243104215933593992
+
+print('Analytic Result :')
+#print('EulerGamma :', EulerGamma)
+print('eps^0 : ',g2_0(s1,mhsq,mtsq))
+print('eps : ',g2_eps(s1,mhsq,mtsq))
+    
+
+
+for name, realp in zip(names, real_parameters):
+    intlib = IntegralLibrary("{0}/{0}_pylink.so".format(name))
+    intlib.use_Qmc(transform="korobov3", fitfunction="polysingular")
+
+    # integrate
+    str_integral_without_prefactor, str_prefactor, str_integral_with_prefactor = intlib(real_parameters=realp)
+
+    # convert complex numbers from c++ to sympy notation
+    str_integral_with_prefactor = str_integral_with_prefactor.replace(',','+I*')
+
+    # convert result to sympy expressions
+    integral_result = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+    integral_result_err = sp.sympify(str_integral_with_prefactor.replace('+/-','*value+error*'))
+
+    # examples how to access individual orders
+    print('Numerical Result')
+    for power in [0, 1]:
+        valreal, valimg = integral_result.coeff('eps',power).coeff('value').as_real_imag()
+        errreal, errimg = integral_result.coeff('eps',power).coeff('error').as_real_imag()
+        print("eps^{:<2} {: .5f}{:+.5f}*I +/- {: .5e}{:+.5e}*I".format(power,float(valreal),float(valimg),float(errreal),float(errimg)))
---
+++ Expansion by regions/g5/mygenerate.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+# in this example we try to expand the dotted loop using the mass of the particle
+# in the loop as the small parameter
+# here we do it both ways, expaning in the msq directly, or replacing it by
+# z*msq, expanding in z and then using z=1, here we use 2 Feynman parameters
+# (i.e. we have two propagators and a powerlist)
+
+from pySecDec.loop_integral import loop_regions
+from pySecDec.code_writer import sum_package
+import pySecDec as psd
+import numpy as np
+
+# define the loop integral for the case where we expand in m
+set1 = [       ('p1*p1', '0'),
+                            ('p2*p2', '0'),
+                            ('p3*p3', '0'),
+                            ('p1*p2', '(s12)/2'),
+                            ('p1*p3', '(s13)/2'),
+                            ('p2*p3', '(s23)/2'),
+                            ('mt**2','mtsq'),
+                            ('(p1*p2)+(p2*p3)+(p1*p3)','(mhsq)/2')
+                        ]
+
+li_m =   psd.loop_integral.LoopIntegralFromPropagators(
+
+    
+
+    propagators = ['(k1)**2 - (mt)**2', '(k1-p1)**2 - (mt)**2', '(k1-p1-p2)**2 - (mt)**2 ', '(k1-p1-p2-p3)**2 - (mt)**2 '],
+    loop_momenta = ['k1'],
+    external_momenta = ['p1','p2','p3'],
+    powerlist = ["1+n1", "1+ n1/2", "1+n1/3", "0+n1/5"],
+
+    regulators=["n1","eps"],
+
+    replacement_rules = set1,
+
+    dimensionality= '4-2*eps'
+
+    )
+
+# define the loop integeral for the case where we expand in z
+li_z = psd.loop_integral.LoopIntegralFromPropagators(
+propagators=li_m.propagators, loop_momenta=li_m.loop_momenta, regulators=li_m.regulators, powerlist=li_m.powerlist,
+replacement_rules = [       ('p1*p1', '0'),
+                            ('p2*p2', '0'),
+                            ('p3*p3', '0'),
+                            ('p1*p2', '(s12*z)/2'),
+                            ('p1*p3', '(s13*z)/2'),
+                            ('p2*p3', '(s23*z)/2'),
+                            ('mt**2','mtsq'),
+                            ('(p1*p2)+(p2*p3)+(p1*p3)','((mhsq)*z)/2')
+                        ])
+
+order=1
+
+# ['s12','s13','s23','mtsq','mhsq','z']
+
+name = "g5_s12_z"
+real_parameters = ['s12','s13','s23','mtsq','mhsq','z']
+smallness_parameter = "z"
+li = li_z
+
+# find the regions and expand the integrals using expansion by regions
+generators_args = loop_regions(
+     name = name,
+     loop_integral=li,
+     smallness_parameter = smallness_parameter,
+     expansion_by_regions_order=order)
+
+# generate code that will calculate the sum of all regions and all orders in
+# the smallness parameter
+sum_package(name, generators_args, li.regulators,
+     requested_orders = [0,1],
+     real_parameters = real_parameters,
+     complex_parameters = [])
+
+
+
+
+
+
+
+
